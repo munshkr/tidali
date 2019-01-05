6428836e30db4e83ab14d25b63e83e17552cee89
alex <alex@slab.org>
2019-01-05 23:18:18 +0000

    fix deps

--- make-haskell-libs.sh
+++ make-haskell-libs.sh
@@ -11,16 +11,28 @@ cp $GHCLIB/llvm-targets $HASKELL_LIBS
 cp $GHCLIB/llvm-passes $HASKELL_LIBS
 
 cp -r $GHCLIB/rts                               $HASKELL_LIBS
-cp -r $GHCLIB/base-* $HASKELL_LIBS
+cp -r $GHCLIB/base-*                            $HASKELL_LIBS
 cp -r $GHCLIB/ghc-prim*                         $HASKELL_LIBS
 cp -r $GHCLIB/integ*                            $HASKELL_LIBS
+cp -r $GHCLIB/containers*                       $HASKELL_LIBS
+cp -r $GHCLIB/array*                            $HASKELL_LIBS
+cp -r $GHCLIB/deepseq*                          $HASKELL_LIBS
+cp -r $GHCLIB/transformers*                     $HASKELL_LIBS
+cp -r $GHCLIB/template-haskell*                 $HASKELL_LIBS
+cp -r $GHCLIB/text*                             $HASKELL_LIBS
+cp -r $GHCLIB/binary*                           $HASKELL_LIBS
+cp -r $GHCLIB/bytestring*                       $HASKELL_LIBS
+cp -r $GHCLIB/time*                             $HASKELL_LIBS
+cp -r $GHCLIB/mtl*                              $HASKELL_LIBS
+cp -r $GHCLIB/ghc-boot*                         $HASKELL_LIBS
+cp -r $GHCLIB/pretty*                           $HASKELL_LIBS
+cp -r $GHCLIB/parsec*                           $HASKELL_LIBS
+cp -r $GHCLIB/stm*                              $HASKELL_LIBS
+cp -r $GHCLIB/unix*                             $HASKELL_LIBS
 
-cp -r $GHCLIB/package.conf.d/rts.conf           $HASKELL_LIBS/package.conf.d/
-cp -r $GHCLIB/package.conf.d/base-*.conf        $HASKELL_LIBS/package.conf.d/
-cp -r $GHCLIB/package.conf.d/ghc-prim-*.conf    $HASKELL_LIBS/package.conf.d/
-cp -r $GHCLIB/package.conf.d/integer-gmp-*.conf $HASKELL_LIBS/package.conf.d/
+cp -r $GHCLIB/package.conf.d/*.conf             $HASKELL_LIBS/package.conf.d/
 
-cp -r $GHCLIB/include                            $HASKELL_LIBS
+cp -r $GHCLIB/include                           $HASKELL_LIBS
 
 cabal sandbox init
 cabal install --only-dependencies
@@ -28,6 +40,7 @@ cabal install --only-dependencies
 cp -r .cabal-sandbox/lib/x86_64-linux-ghc-8.6.3/* $HASKELL_LIBS
 cp -r .cabal-sandbox/*-packages.conf.d/*.conf $HASKELL_LIBS/package.conf.d/
 
+# perl -p -i -e 's!(/home/alex/src/tidali/.cabal-sandbox/lib/x86_64-linux-ghc-8.6.3|/home/alex/.ghcup/ghc/8.6.3/lib/ghc-8.6.3)!\$\{pkgroot\}!g' \
 perl -p -i -e 's!(/home/alex/src/tidali/.cabal-sandbox/lib/x86_64-linux-ghc-8.6.3|/home/alex/.ghcup/ghc/8.6.3/lib/ghc-8.6.3)!\$\{pkgroot\}!g' \
     $HASKELL_LIBS/package.conf.d/*.conf
 
